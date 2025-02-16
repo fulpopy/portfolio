21a7816b460b0143f52a12d36d4d111debbb2c84
fulpopy <akash16799@gmail.com>
2025-02-16 23:30:44 +0530

    Add header and main components; update configuration and dependencies
    - Introduced Header and Main components for improved layout.
    - Updated next.config.ts to allow images from GitHub.
    - Added react-icons dependency for icon usage.
    - Refactored page structure in page.tsx to utilize new components.
    - Cleaned up globals.css by removing unused styles.
    - Created constants for user and navigation items.

---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { useState } from "react";
+import { NavItems } from "@/models/NavItem";
+import { user, navItems } from "../constants/constant";
+import { BiMenu } from "react-icons/bi";
+const Header: React.FC = () => {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+  return (
+    <header className="bg-white p-6 justify-between w-full z-10 fixed top-0 md:flex">
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-bold">{user.name}</h1>
+        <BiMenu
+          className="md:hidden cursor-pointer hover:bg-gray-400 hover:rounded-full p-1"
+          size={40}
+          onClick={() => setShowMenu((prevState) => !prevState)}
+        />
+      </div>
+      <div
+        className={`mr-8 md:space-x-6 mt-3 md:mt-0 md:block ${
+          showMenu ? "block" : "hidden"
+        } flex flex-col md:flex-row space-y-2 md:space-y-0 align-right items-end`}
+      >
+        {Object.keys(navItems).map((item) => (
+          <a
+            className="block md:inline-block cursor-pointer"
+            key={navItems[item as keyof NavItems].label}
+          >
+            {navItems[item as keyof NavItems].label}
+          </a>
+        ))}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
