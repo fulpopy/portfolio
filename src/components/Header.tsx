6998ae42a391fc5d15d3cc6bb04c8c6fdffbabd6
fulpopy <akash16799@gmail.com>
2025-02-18 23:32:41 +0530

    Implement click outside functionality to close menu in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,30 @@
 "use client";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { NavItems } from "@/models/NavItem";
 import { user, navItems } from "../constants/constant";
 import { BiMenu } from "react-icons/bi";
 import { Link as ScrollLink } from "react-scroll";
 const Header: React.FC = () => {
   const [showMenu, setShowMenu] = useState<boolean>(false);
+  const headerRef = useRef<HTMLInputElement>(null);
+  const onClickOutside = (e: MouseEvent) => {
+    if (headerRef.current && !headerRef.current.contains(e.target as Node)) {
+      setShowMenu(false);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("mousedown", onClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", onClickOutside);
+    };
+  }, [showMenu]);
   return (
-    <header className="bg-white p-6 justify-between w-full z-10 fixed top-0 md:flex">
+    <header
+      ref={headerRef}
+      className="bg-white p-4 md:p-6 justify-between w-full z-10 fixed top-0 md:flex"
+    >
       <div className="flex justify-between">
-        <h1 className="text-2xl font-bold">{user.name}</h1>
+        <h1 className="text-2xl font-bold flex items-center">{user.name}</h1>
         <BiMenu
           className="md:hidden cursor-pointer hover:bg-gray-400 hover:rounded-full p-1"
           size={40}
@@ -28,6 +43,7 @@ const Header: React.FC = () => {
             className="block md:inline-block cursor-pointer"
             spy={true}
             key={navItems[item as keyof NavItems].label}
+            onClick={() => setShowMenu(false)}
           >
             {navItems[item as keyof NavItems].label}
           </ScrollLink>
